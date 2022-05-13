2fa273e4c0845a5165ee681fe42063af7476ecb8
DannOut <danielxdn@gmail.com>
2022-05-13 15:30:46 -0300

    estudo de for-of

---
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/2.Arrays-LoopFor/for-of/learning-for.js
@@ -0,0 +1,11 @@
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
+
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index<groceryList.length; index +=1){
+  console.log ('o item '+ (index + 1) + ' da lista de compra é:' + groceryList[index]);
+}
