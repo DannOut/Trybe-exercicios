878d899dacb080270a10b3ce9fa90f9d1e26fc99
DannOut <danielxdn@gmail.com>
2022-05-17 14:13:16 -0300

    teste.ignorar

---
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/teste.js
@@ -0,0 +1,19 @@
+
+
+function generateNumberTriangle(n) {
+    let midpoint = Math.floor(n / 2); // Middle of pyramid.
+    let level = ''; // will be reset each level loop
+
+    for(var i = 0; i < (n/2); i++) { // Looping through levels
+        level = '';
+        for(var j = 0; j < n; j++) {
+            if(j < midpoint-i || j > midpoint+i) {
+                level += ' ';
+            } else {
+                level += '*';
+            }
+        }
+        console.log(level);
+    }
+}
+generateNumberTriangle(5)
