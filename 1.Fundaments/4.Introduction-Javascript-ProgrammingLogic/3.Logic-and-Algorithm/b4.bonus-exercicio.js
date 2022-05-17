5a572267bdae922920314801c7df16831168f7d8
DannOut <danielxdn@gmail.com>
2022-05-17 13:59:41 -0300

    verificar b4

--- 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/3.Logic-and-Algorithm/b4.bonus-exercicio.js
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/3.Logic-and-Algorithm/b4.bonus-exercicio.js
@@ -9,7 +9,23 @@ n = 5
 
 */
 
-function pyramid(n) {
-  
+var n = 5;
+generateNumberTriangle(n);
+
+function generateNumberTriangle(n) {
+    let width = (2 * n) -1; // Always the case.
+    let midpoint = Math.floor(width / 2); // Middle of pyramid.
+    let level = ''; // will be reset each level loop
+
+    for(var i = 0; i < n; i++) { // Looping through levels
+        level = '';
+        for(var j = 0; j < width; j++) {
+            if(j < midpoint-i || j > midpoint+i) {
+                level += '.';
+            } else {
+                level += '#';
+            }
+        }
+        console.log(level);
+    }
 }
-pyramid(5)
