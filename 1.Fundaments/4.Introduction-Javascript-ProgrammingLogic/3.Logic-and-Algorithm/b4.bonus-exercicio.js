38a1bf128b334d43d47c7a35b638233b383e7647
DannOut <danielxdn@gmail.com>
2022-05-17 13:50:03 -0300

    Atualização b4

--- 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/3.Logic-and-Algorithm/b4.bonus-exercicio.js
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/3.Logic-and-Algorithm/b4.bonus-exercicio.js
@@ -10,18 +10,6 @@ n = 5
 */
 
 function pyramid(n) {
-  for (let index = 1; index <= n; index += 1) {
-    let armazenamento = '';
-    if (!(index % 2) == 0) {
-      for (let index2 = 1; index2 <= n; index2 += 1) {
-        armazenamento += '*';
-        value = index2 ;
-        if (!(index2 % 2) == 0) {
-          console.log(value);
-        }
-      }
-      console.log('');
-    }
-  }
+  
 }
-pyramid(5);
+pyramid(5)
