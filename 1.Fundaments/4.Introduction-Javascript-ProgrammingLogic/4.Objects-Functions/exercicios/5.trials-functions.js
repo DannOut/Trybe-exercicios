b8e78b4b7d41929a26087ca952caf0728ec36054
DannOut <danielxdn@gmail.com>
2022-05-18 11:19:09 -0300

    travei nesse exercício, a partir daqui, irei recomeçar ele

--- 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/exercicios/5.trials-functions.js
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/exercicios/5.trials-functions.js
@@ -11,19 +11,26 @@ Valor esperado no retorno da função: 2.
 
 function maxRepeatedNumber(array) {
   for (let index = 0; index < array.length; index += 1) {
-    console.log('Corrida: ', index);
+    //console.log('Corrida: ', index);
     // até aqui, estou apenas correndo dentro do array
-    checagem = 0; // precisa resetar a cada nova corrida
-
+    check = 0; // precisa resetar a cada nova corrida
+    temp2 = []; // armazena o valor de array[sum]
+    if (temp2.length <= temp2.length){
     for (let sum = 0; sum < array.length; sum += 1) {
       //checagem de valores dentro do array
       temp = array[index]; // temporário está guardando o valor de cada corrida.
       if (temp == array[sum]) {
         // incrementando 1 caso ache um novo valor no array
-        checagem += 1;
+        check += 1;
+        temp2.push (array[sum]) // guarda o valor do maior numero repetido do array.
+        //console.log (temp2);
       }
     }
-    console.log(checagem);
+   // temp2.reverse(); // 
+    console.log('O maior valor: ',temp2[]);
   }
+  }
+
+
 }
 maxRepeatedNumber([2, 3, 2, 5, 8, 2, 3]);
