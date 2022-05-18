c343617593532e8049f8d6c04efd8fda183217c6
DannOut <danielxdn@gmail.com>
2022-05-18 13:45:17 -0300

    FOI SÓ PARAR PRA RELAXAR QUE A IDEIA VEIO CARALHO

--- 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/exercicios/5.trials-functions.js
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/exercicios/5.trials-functions.js
@@ -10,10 +10,20 @@ Valor esperado no retorno da função: 2.
 //encontrar a quantidade de vezes que o numero se repete dentro do array
 
 function maxRepeatedNumber(array) {
+  let guardHigherNumber = 0;
   for (let index = 0; index < array.length; index += 1) {
-
-    
-}}
+    let checker = 0;
+    let count = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      checker = array[index];
+      if (checker == array[i]) {
+        count += 1;
+      }
+    }
+    if (guardHigherNumber < count) {
+      guardHigherNumber = array[index];
+    }
+  }
+  console.log(guardHigherNumber);
+}
 maxRepeatedNumber([2, 3, 2, 5, 8, 2, 3]);
-
-
