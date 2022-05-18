6bd528ff42159dea9ad29bfb222256fd187f1422
DannOut <danielxdn@gmail.com>
2022-05-18 11:31:58 -0300

    até aqui, consigo achar quantas vezes foi repetida cada corrida

--- 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/exercicios/5.trials-functions.js
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/exercicios/5.trials-functions.js
@@ -7,30 +7,21 @@ Valor esperado no retorno da função: 2.
 
 //criar uma função
 //fazer um loop para percorrer o array
-//
+//encontrar a quantidade de vezes que o numero se repete dentro do array
+
 
 function maxRepeatedNumber(array) {
   for (let index = 0; index < array.length; index += 1) {
-    //console.log('Corrida: ', index);
-    // até aqui, estou apenas correndo dentro do array
-    check = 0; // precisa resetar a cada nova corrida
-    temp2 = []; // armazena o valor de array[sum]
-    if (temp2.length <= temp2.length){
-    for (let sum = 0; sum < array.length; sum += 1) {
-      //checagem de valores dentro do array
-      temp = array[index]; // temporário está guardando o valor de cada corrida.
-      if (temp == array[sum]) {
-        // incrementando 1 caso ache um novo valor no array
-        check += 1;
-        temp2.push (array[sum]) // guarda o valor do maior numero repetido do array.
-        //console.log (temp2);
-      }
+    check = 0;
+    //console.log ('valor do indice: ',index);
+    for (let run = 0; run < array.length; run +=1){
+     // console.log ('corridas no array numero ',index, ' : ',run)
+     if (array[index] == array[run]){
+       check +=1
+       
+     }
+     
     }
-   // temp2.reverse(); // 
-    console.log('O maior valor: ',temp2[]);
-  }
-  }
-
-
-}
+    console.log(check);
+}}
 maxRepeatedNumber([2, 3, 2, 5, 8, 2, 3]);
