55428c2ec2f970060b2a7fa9190587c73f8b6760
DannOut <danielxdn@gmail.com>
2022-05-18 07:55:04 -0300

    Exercicios bloco 4.4

---
+++ 1.Fundaments/4.Introduction-Javascript-ProgrammingLogic/4.Objects-Functions/exercicios/4.tritals-functions.js
@@ -0,0 +1,17 @@
+/* 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+Valor esperado no retorno da função: Fernanda.  */
+
+//criar uma função que receba um array
+//percorrer todo o array de nomes 
+//percorrer cada palavra do array
+//retornar tamanho da palavra do array
+//armazenar o tamanho da palabra numa variavel
+// comparar com o valor seguinte
+//ao final, retornar a maior palabra dentro do array
+
+function higherWord(array){
+
+
+}
+higherWord(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
