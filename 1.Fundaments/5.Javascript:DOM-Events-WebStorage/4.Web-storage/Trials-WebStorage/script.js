6d4f9775b253039c1cedd2cdf684d190fe2760d2
DannOut <danielxdn@gmail.com>
2022-05-31 11:27:28 -0300

    exercicios

--- 1.Fundaments/5.Javascript:DOM-Events-WebStorage/4.Web-storage/Trials-WebStorage/script.js
+++ 1.Fundaments/5.Javascript:DOM-Events-WebStorage/4.Web-storage/Trials-WebStorage/script.js
@@ -1,29 +1,75 @@
+const limitSquare = document.getElementsByClassName('quadrado');
+const containerId = document.querySelector('#container');
+const buttonCreateDiv = document.getElementsByClassName('botaoDiv');
+const getDivs = document.getElementById('base');
+const getTagP = document.getElementsByTagName('p');
+
+function povoateTagP(){
+  for (let keys of getTagP){
+    keys.className = 'paragrafos'
+  }
+} povoateTagP ();
+
 function creatingInput() {
-  const containerId = document.querySelector('#container');
   const createInput = document.createElement('input');
-  createInput.placeholder = 'enter some text';
+  createInput.placeholder = 'Digite uma cor !';
+  createInput.id = 'input';
   createInput.style.display = 'block';
   createInput.style.width = '400px';
   containerId.appendChild(createInput);
 }
 creatingInput();
 
-function creatingButton() {
-  const containerId = document.querySelector('#container');
+function creatingButtonColorCreator() {
   const createButton = document.createElement('button');
-  createButton.innerText = 'Aperte aqui!';
-  createButton.className = 'botao'
+  createButton.innerText = 'Random Color';
+  createButton.className = 'botaoDiv';
   createButton.style.display = 'block';
+  createButton.style.alignSelf = 'center';
   createButton.style.padding = '10px';
   createButton.style.marginTop = '5px';
   containerId.appendChild(createButton);
 }
-creatingButton();
+creatingButtonColorCreator();
 
- let array = [1,2,3,4,5,6,7,8,9,10];
+function creatingButtonReset() {
+  const createButton = document.createElement('button');
+  createButton.innerText = 'Reset';
+  createButton.className = 'botao2';
+  createButton.style.display = 'block';
+  createButton.style.alignSelf = 'center';
+  createButton.style.padding = '10px';
+  createButton.style.marginTop = '5px';
+  containerId.appendChild(createButton);
+}
+creatingButtonReset();
 
- 
- function creatingDivs(event)
+// cria um quadrado de cor aleatoria dentro da div
+buttonCreateDiv[0].addEventListener('click', function (event) {
+const createDivs = document.createElement('div');
+let bkdRandom = Math.floor(Math.random()*16777215).toString(16);
+  createDivs.style.backgroundColor ='#'+bkdRandom;
+  createDivs.classList = 'quadrado'; 
+  // parar quando chegar a 25
+  const getTotalDivs = document.getElementsByClassName('quadrado');
+  if (getTotalDivs.length < 25){
+    event.target.getButton = getDivs.appendChild(createDivs);
+  } else alert('Limite máximo de cores atingido')
+})
 
+const input = document.getElementById('input');
+input.addEventListener('keydown', function (event) {
+  const changeBody = document.getElementsByTagName('body');
+  changeBody[0].style.backgroundColor = event.target.value;
+});
 
+const getClassP = document.getElementsByClassName('paragrafos')
+const getQuadrado = document.getElementsByClassName('quadrado');
+getDivs.addEventListener('click', function(event){
+let getQuadradoColor = event.target;
+getQuadradoColor.classList = 'quadrado pandora';
+getQuadradoColor.classList.pandora = getQuadrado;
+
+  console.log(getQuadradoColor);
+})
 
