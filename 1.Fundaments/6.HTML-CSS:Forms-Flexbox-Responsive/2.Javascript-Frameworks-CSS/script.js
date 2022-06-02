3f95a7af283c6c8aeb83214cddb6cb3a34c84fc5
DannOut <danielxdn@gmail.com>
2022-06-02 09:54:36 -0300

    exercicios do dia + estudo de frameworks

--- 1.Fundaments/6.HTML-CSS:Forms-Flexbox-Responsive/2.Javascript-Frameworks-CSS/script.js
+++ 1.Fundaments/6.HTML-CSS:Forms-Flexbox-Responsive/2.Javascript-Frameworks-CSS/script.js
@@ -1,5 +1,51 @@
-const send = document.querySelector('#send');
+const validation = new JustValidate('#form');
 
-send.addEventListener('click', (event)=>{
-  event.preventDefault();
-})
+// Validation
+validation
+  .addField('#username', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Username is required',
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
+validation
+  .addField('#response', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 500,
+    },
+  ])
+  .addRequiredGroup('#placesDiv');
+
+validation
+  .addField('#offers1', [
+    {
+      rule: 'required',
+      errorMessage: 'confirme os termos de contrato',
+    },
+  ])
+  .onSuccess((event) => {
+    console.log('Validation passes and form submitted', event);
+  });
