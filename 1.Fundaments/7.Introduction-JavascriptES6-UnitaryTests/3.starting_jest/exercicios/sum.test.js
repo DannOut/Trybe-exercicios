f7600b603173993568a34fe3a2639f25941acb78
DannOut <danielxdn@gmail.com>
2022-06-19 09:06:12 -0300

    aprendendo tests

--- 1.Fundaments/7.Introduction-JavascriptES6-UnitaryTests/3.starting_jest/exercicios/sum.test.js
+++ 1.Fundaments/7.Introduction-JavascriptES6-UnitaryTests/3.starting_jest/exercicios/sum.test.js
@@ -2,6 +2,6 @@ const sum = require('./sum.js');
 
 describe('Training Sum test', () => {
   it('A função recebe a soma dos parâmetros A + B', () => {
-    expect(sum(2, 5)).toEqual(7);
+    expect(sum(2, 5)).toequal(7);
   });
 });
