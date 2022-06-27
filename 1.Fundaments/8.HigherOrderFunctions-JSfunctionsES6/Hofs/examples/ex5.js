d77cd40d582d71c1265986418bfaf322e66f30cd
DannOut <danielxdn@gmail.com>
2022-06-27 15:46:18 -0300

    examples done

---
+++ 1.Fundaments/8.HigherOrderFunctions-JSfunctionsES6/Hofs/examples/ex5.js
@@ -0,0 +1,26 @@
+var pilots = [
+  {
+    id: 10,
+    name: "Poe Dameron",
+    years: 14,
+  },
+  {
+    id: 2,
+    name: "Temmin 'Snap' Wexley",
+    years: 30,
+  },
+  {
+    id: 41,
+    name: "Tallissan Lintra",
+    years: 16,
+  },
+  {
+    id: 99,
+    name: "Ello Asty",
+    years: 22,
+  }
+];
+
+
+const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years);
+console.log(totalYears);
