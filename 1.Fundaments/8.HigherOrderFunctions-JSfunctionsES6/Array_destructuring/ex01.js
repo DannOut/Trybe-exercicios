3f7a835322536e3c318bd67f2aca321f1b3e50ad
DannOut <danielxdn@gmail.com>
2022-06-28 18:43:21 -0300

    update

---
+++ 1.Fundaments/8.HigherOrderFunctions-JSfunctionsES6/Array_destructuring/ex01.js
@@ -0,0 +1,14 @@
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+console.log(firstCountry); // Brazil
+console.log(secondCountry); // Japan
+console.log(thirdCountry); // China
+console.log(fourthCountry); // Canada
+
+
+
+
+
+const str = 'daniel ( outeiro ) ( dos ) (santos)';
+console.log(str.replace(/[\(|\)]+/g, ''))
