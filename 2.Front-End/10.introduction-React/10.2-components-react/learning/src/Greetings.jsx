8851b5f4c6192f98dbf7ff62074fda94bba3a5e0
DannOut <danielxdn@gmail.com>
2022-07-19 14:35:32 -0300

    starting Components React

---
+++ 2.Front-End/10.introduction-React/10.2-components-react/learning/src/Greetings.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <h1>
+        Hello, {this.props.name} {this.props.lastName}
+      </h1>
+    );
+  }
+}
+
+export default Greeting;
