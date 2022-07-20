975129456666f1aad8b19add250fc7e69580828f
DannOut <danielxdn@gmail.com>
2022-07-19 22:17:57 -0300

    starting Components React

--- 2.Front-End/10.introduction-React/10.2-components-react/exercises1/src/components/Pokemon.jsx
+++ 2.Front-End/10.introduction-React/10.2-components-react/exercises1/src/components/Pokemon.jsx
@@ -1,14 +1,16 @@
 import { Component } from 'react';
 class Pokemon extends Component {
   render() {
-    const {name, id, type, image, moreInfo} = this.props
+    const { name, id, type, image, moreInfo } = this.props;
     return (
-      <div className='pokemon-div' >
+      <div className='pokemon-div pokemon-flip'>
         <h1 className='pokemon-name'>{name}</h1>
         <h2 className='pokemon-id'>#{id}</h2>
         <h3 className='pokemon-type'>{type}</h3>
         <img className='pokemon-img' src={image} alt={name} />
-        <a className='pokemon-link' href={moreInfo}>Clique aqui</a>
+        <a className='pokemon-link' href={moreInfo}>
+          Wikia
+        </a>
       </div>
     );
   }
