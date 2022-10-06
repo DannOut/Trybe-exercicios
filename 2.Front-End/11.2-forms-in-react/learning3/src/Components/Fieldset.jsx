7752b0497982a5089fdb3944da054573d99d1160
DannOut <danielxdn@gmail.com>
2022-10-06 10:13:16 -0300

    repairs

--- 2.Front-End/11.2-forms-in-react/learning3/src/Components/Fieldset.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Fieldset extends Component {
-  render() {
-    const { children, className } = this.props;
-    const classname = `FieldSet ${className}`
-    return <fieldset className={classname} >{children}</fieldset>;
-  }
-}
--- 2.Front-End/11.2-forms-in-react/learning3/src/Components/Functions/maxCharacterHandler.js
+++
@@ -1,8 +0,0 @@
-const maxCharacterHandler = (maxLength, value) => {
-  let error = undefined
-  if(value.length > maxLength ) error = `Máximo de ${maxLength} caractéres`;
-  return error ? alert(error) : ""
-}
-export default maxCharacterHandler;
-
-// verificar
