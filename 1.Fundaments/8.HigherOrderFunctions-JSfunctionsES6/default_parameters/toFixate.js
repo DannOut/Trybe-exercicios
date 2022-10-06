a4eee9318e5320963cadcae93e423d633128453e
DannOut <danielxdn@gmail.com>
2022-10-06 10:13:00 -0300

    repairs

--- 1.Fundaments/8.HigherOrderFunctions-JSfunctionsES6/default_parameters/toFixate.js
+++
@@ -1,6 +0,0 @@
-const multiply = (number, value = 2) => {
-return number * value
-
-};
-
-console.log(multiply(8));
--- 1.Fundaments/8.HigherOrderFunctions-JSfunctionsES6/exercises/2.js
+++
@@ -1,3 +0,0 @@
-// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
-const sum = (...value) => value.reduce((acc, curr) => (acc += curr), 0);
-console.log(sum(1,2,3,4,5,6,7,8,9));
