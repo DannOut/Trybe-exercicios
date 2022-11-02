fb01cabf7612ddff5760123afa44b7bce82f94b5
DannOut <danielxdn@gmail.com>
2022-11-02 18:57:15 -0300

    Express + MySql

--- 4.5-Node.js_Express-Mysql/trybecash-api/src/server.js
+++ 4.5-Node.js_Express-Mysql/trybecash-api/src/server.js
@@ -1,3 +1,5 @@
+//! PRECISA SER A PRIMEIRA COISA
+require('dotenv').config();
 const app = require('./app');
 // const connection = require('./db/connection');
 
@@ -5,7 +7,7 @@ const port = 3000;
 
 app.listen(port, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${port}`);
-  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
+  // console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
   //depois de testado com npm start, esse arquivo deve ser removido, já que está funcionando
   // const [result] = await connection.execute('SELECT 1');
   // if (result) {
