8d7ca66c587e3d081b25af1052ae7b0f657e0c59
DannOut <danielxdn@gmail.com>
2022-11-03 15:08:27 -0300

    Express + MySql

--- 4.5-Node.js_Express-Mysql/trybecash-api/src/db/peopleDB.js
+++ 4.5-Node.js_Express-Mysql/trybecash-api/src/db/peopleDB.js
@@ -4,31 +4,28 @@ const insert = (person) =>
   connection.execute(
     `INSERT INTO people (first_name, last_name, email, phone) 
       VALUES (?, ?, ?, ?)`,
-    [person.firstName, person.lastName, person.email, person.phone]
+    [person.firstName, person.lastName, person.email, person.phone],
   );
 
 const findAll = () => {
-  //! por que o result do video está com desestrutura em array
-  const result = connection.execute(`SELECT * FROM people`);
+  const result = connection.execute('SELECT * FROM people');
   return result;
 };
 
-const findById = (id) => {
-  return connection.execute(`SELECT * FROM people WHERE id = ?`, [id]);
-};
+const findById = (id) => connection.execute('SELECT * FROM people WHERE id = ?', [id]);
 
 const update = (person, id) => {
   const result = connection.execute(
-    `UPDATE people SET first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ? `,
-    [person.firstName, person.lastName, person.email, person.phone, id]
+    'UPDATE people SET first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ? ',
+    [person.firstName, person.lastName, person.email, person.phone, id],
   );
   console.log(result);
   return result;
 };
 
 const remove = (id) => {
-  const teste = connection.execute(`DELETE FROM people WHERE id = ?`, [id])
-  return teste
+  const teste = connection.execute('DELETE FROM people WHERE id = ?', [id]);
+  return teste;
 };
 
 module.exports = {
@@ -36,5 +33,5 @@ module.exports = {
   findAll,
   findById,
   update,
-  remove
+  remove,
 };
