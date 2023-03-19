c849a154097a12b8be02b5c7ba7faed31d142021
DannOut <danielxdn@gmail.com>
2023-03-19 17:18:23 -0300

    studying python

---
+++ intro_python/exercicios/03-draw.py
@@ -0,0 +1,3 @@
+def draw_squares(n):
+    for row in range(n):
+        print(n * '*')
---
+++ intro_python/exercicios/bonus01.py
@@ -0,0 +1,16 @@
+# def minimum(numbers):
+#     smaller = numbers[0]
+#     for number in numbers:
+#         if number < smaller:
+#             smaller = number
+#     return smaller
+
+# OU
+
+
+def minimum(numbers):
+    return min(numbers)
+
+
+# ou mesmo
+# minimum = min
