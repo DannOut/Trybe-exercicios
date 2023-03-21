e2486a5331ffa242114788b7940cde8806f67535
DannOut <danielxdn@gmail.com>
2023-03-21 16:18:22 -0300

    studying python

---
+++ IO-data/examples/packages_examples.py
@@ -0,0 +1,12 @@
+import http  # importa o pacote http como um módulo
+
+from http import client  # importa o módulo client do pacote http
+
+from http.client import HTTP_PORT
+# importa a constante HTTP_POST do módulo client do pacote http
+
+print(http)
+print('____________________________')
+print(client)
+print('____________________________')
+print(HTTP_PORT)
